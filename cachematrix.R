7db2b1adb6263a3b34189f62ff5648d6c222167f
Kamlesh M Patel <kamalmpatel@gmail.com>
2014-12-16 11:37:08 +0530

    Submission of assignment
    This function find an inverse of matrix X

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## to find the inverse of a matrix and cache it using a free floating variable
 
-## Write a short comment describing this function
+## It contains four function: get, set, get_matrix, set_matrix & special vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m1<-NULL
+  set<-function(y){
+    x<<-y
+    m1<<-NULL
+  }
+  get<-function() x
+  set_matrix<-function(solve) m1<<- solve
+  get_matrix<-function() m1
+  list(set=set, get=get,
+       setmatrix=set_matrix,
+       getmatrix=get_matrix)
 }
 
 
-## Write a short comment describing this function
+## It return the inverse matrix
 
 cacheSolve <- function(x, ...) {
+        m1<-x$getmatrix()
+        if(!is.null(m1)){
+                message("cached data available")
+                return(m1)
+        }
+        matrix<-x$get()
+        m1<-solve(matrix, ...)
+        x$setmatrix(m1)
+ 
         ## Return a matrix that is the inverse of 'x'
+        m1
 }
